0c83584aa59b4dc923841799e984b3f39ddd809a
tobinskaiauliana <Ulianafencing@mail.ru>
2025-11-01 22:41:40 +0300

    Homework 4

--- presentations/fastapi_app.py
+++ presentations/fastapi_app.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI, HTTPException, Response, status
+from fastapi import FastAPI, HTTPException, Response, status, Request
 from pydantic import BaseModel
 from services.link_service import LinkService
-
+import time
+from loguru import logger
 
 def create_app() -> FastAPI:
     app = FastAPI()
+
+    @app.middleware("http")
+    async def add_latency_header(request: Request, call_next) -> Response:
+        t0 = time.time()
+        response = await call_next(request)
+        elapsed_ms = round((time.time() - t0) * 1000, 2)
+        response.headers["X-Latency"] = str(elapsed_ms)
+        logger.debug("{} {} done in {}ms", request.method, request.url.path, elapsed_ms)
+        return response
+
     short_link_service = LinkService()
 
     class PutLink(BaseModel):
@@ -15,9 +26,12 @@ def create_app() -> FastAPI:
 
     @app.post("/link")
     def create_link(put_link_request: PutLink) -> PutLink:
-        short_link = short_link_service.create_link(put_link_request.link)
-        return PutLink(link=_service_link_to_real(short_link))
-
+        try:
+            short_link = short_link_service.create_link(put_link_request.link)
+            return PutLink(link=_service_link_to_real(short_link))
+        except Exception as e:
+            logger.exception("exception.raised")
+            raise HTTPException(status_code=422, detail=str(e))
     @app.get("/{link}")
     def get_link(link: str) -> Response:
         real_link = short_link_service.get_real_link(link)
@@ -28,3 +42,4 @@ def create_app() -> FastAPI:
         return Response(status_code=status.HTTP_301_MOVED_PERMANENTLY, headers={"Location": real_link})
 
     return app
+
--- services/link_service.py
+++ services/link_service.py
@@ -1,11 +1,25 @@
 from utils.utils_random import random_alfanum
-
+import re
 
 class LinkService:
     def __init__(self) -> None:
         self.short_link_to_real_link: dict[str, str] = {}
 
+    def add_https(self, url: str) -> str:
+        if not url.startswith(('http://', 'https://')):
+            return 'https://' + url
+        return url
+
+    def valid(self, url: str) -> bool:
+        pattern=r'https?://[a-zA-Z0-9.-]+\.[a-zA-Z]+'
+        if re.match(pattern,url):
+            return True
+        else: return False
+
     def create_link(self, link: str) -> str:
+        link=self.add_https(link)
+        if not self.valid(link):
+            raise ValueError("422")
         short_link = random_alfanum(5)
         self.short_link_to_real_link[short_link] = link
 
